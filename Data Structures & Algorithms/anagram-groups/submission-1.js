859a70af7bfdeb12c992a43332a4bea7f42b0696
Stan Ionut-Raul <146850246+stanionutraul@users.noreply.github.com>
2026-04-04 16:51:06 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = new Map();
+        for (let word of strs){
+            let key = word.split("").sort().join('');
+
+            if(!map.has(key)){
+                map.set(key, [])
+            }
+            map.get(key).push(word);
+        }
+        return Array.from(map.values());
+    }
+}
