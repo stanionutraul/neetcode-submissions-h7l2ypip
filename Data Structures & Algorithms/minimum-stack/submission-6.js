807cc6147c4ef8171aeedc8d673ee52d9dd6e349
Stan Ionut-Raul <146850246+stanionutraul@users.noreply.github.com>
2026-04-14 13:30:44 +0300

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.js
@@ -0,0 +1,37 @@
+class MinStack {
+    constructor() {
+
+    this.stack = [];
+    this.minStack = [];
+    }
+    push(val) {
+        if(this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]){
+            this.minStack.push(val);
+        }
+        this.stack.push(val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        if(this.stack[this.stack.length-1] === this.minStack[this.minStack.length-1]){
+            this.minStack.pop()
+        }
+        this.stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length-1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length-1];
+    }
+}
